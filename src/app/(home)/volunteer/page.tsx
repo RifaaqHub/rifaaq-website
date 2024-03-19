f4d11141ddd3ee9665dd474ac4ee96368a483b10
reedwane <abdulkareemtemmy07@gmail.com>
2024-03-19 07:52:29 +0100

    redirect for volunteer

---
+++ src/app/(home)/volunteer/page.tsx
@@ -0,0 +1,9 @@
+import { redirect } from 'next/navigation'
+
+const VolunteerForm = () => {
+  redirect(
+    'https://docs.google.com/forms/d/e/1FAIpQLSf7dGPS_jglnUSt3RAlY17jGJwQCRunaaC5WGcdxATjAYtOdQ/viewform',
+  )
+}
+
+export default VolunteerForm
