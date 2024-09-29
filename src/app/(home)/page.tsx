fd7446b3dd0d281a5eb3f3fe6784cb10aa48a595
Ridwan Gboyega Abdulkareem <abdulkareemtemmy07@gmail.com>
2024-09-29 09:11:58 +0100

    Update page.tsx

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,18 +1,24 @@
-'use client'
+//'use client'
 
-import React from 'react'
-import Hero from './hero'
-import About from './about'
-import Offerings from './offerings'
-import NewsLetterForm from './newsletterForm'
+//import React from 'react'
+//import Hero from './hero'
+//import About from './about'
+//import Offerings from './offerings'
+//import NewsLetterForm from './newsletterForm'
+
+import { redirect } from 'next/navigation'
+ 
+
 
 const Home = () => {
+if (true) redirect('https://acco.rifaaq.com')
+
   return (
     <>
-      <Hero />
+      {/<Hero />
       <About />
       <Offerings />
-      <NewsLetterForm />
+      <NewsLetterForm />/} 
     </>
   )
 }
